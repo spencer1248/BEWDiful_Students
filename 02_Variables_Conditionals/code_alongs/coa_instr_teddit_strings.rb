7eea528d14ae45871355ae5982348f260afc01a8
silverballs <asilbersmith11@gmail.com>
2013-07-18 16:33:15 -0400

    updated some files

---
+++ 02_Variables_Conditionals/code_alongs/coa_instr_teddit_strings.rb
@@ -0,0 +1,21 @@
+#TEDDIT:  Strings
+
+#Version 1 - Welcome the user to Teddit, print the first story to the terminal.
+###########
+###########
+
+#Version 2 - Replace the story title and upvotes with variables.
+###########
+###########
+
+#Version 3 - Add data type casting (fix casting issue).
+###########
+###########
+
+#Version 4 - Use string interpolation and escape characters, polish the output.
+###########
+###########
+
+#Version 5 - Adding methods
+###########
+###########
