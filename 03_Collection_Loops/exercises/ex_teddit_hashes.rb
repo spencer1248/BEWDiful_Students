f20d8dd1e86997cd7d7900c9a8ba079c089cbed6
silverballs <asilbersmith11@gmail.com>
2013-07-28 16:15:32 -0400

    changed a lot.  everything working but need to define some new methods to make better,  for the sake of moving forwrad to next excersise this will work fine

--- 03_Collection_Loops/exercises/ex_teddit_hashes.rb
+++ 03_Collection_Loops/exercises/ex_teddit_hashes.rb
@@ -2,17 +2,17 @@
 # Step 1: Working with Arrays and Hashes
 
 # Copy your solution from conditional_teddit.rb. 
-# Create an empty stories array. This will be used to hold all stories we capture.
+# Create an empty d array. This will be used to hold all d we capture.
 # # Users enter the story as they do now, but it gets put in a hash instead. 
 # Update any reference to the story (upvotes, category and title) 
 # Your story hash should look like the one below: 
 # { title: "Monkeys thank mayor for flounder tooth necklace", category: "Teeth", upvotes: 1 }
-# Add the hash to an array called stories and print "Story: Monkeys thank mayor for flounder tooth necklace, Category: (Teeth), Current Upvotes: 1"
-# Using the stories array 
+# Add the hash to an array called d and print "Story: Monkeys thank mayor for flounder tooth necklace, Category: (Teeth), Current Upvotes: 1"
+# Using the d array 
 # Test your cat, bacon, and food upvote conditional logic.
 #
 # Step 2: Adding Loops 
-# Use a loop, so that your program continues to ask a user for stories until they chose to quit. ("Would you like to add another story? Enter 'y' or 'n'")
+# Use a loop, so that your program continues to ask a user for d until they chose to quit. ("Would you like to add another story? Enter 'y' or 'n'")
 # Once the user is finished with entering their stories, use .each to print each story in the stories array. 
 #
 
@@ -22,45 +22,53 @@
 
 
 
-def prompt_new_story
-	story = {} # do you prefer using story = Hash.new for better reading or other purposes?  please share  your thoughts
-	puts "What is the title of your new story?"
-	story[:title] = gets.chomp.to_s
-	puts "What category does your story fall under?"
-	story[:category] = gets.chomp.to_s  # do I need to use the .to_s method here?  I think that gets.chomp will automatically make it a string, right?
-	# story = { :title => story[:title], :category => story[:category] } # this is what return story knows. 
-    return story
+
+def get_upvotes(story)
+	story[:upvotes] = 1
+	if story[:title].include? "hey"
+		story[:upvotes] *= 5
+	elsif story[:title].include? "you"
+		story[:upvotes] *= 10
+	else story[:upvotes] = 1
+	end
+end
+
+def display_new_story(story)
+	puts "New Story Added!!! #{story[:title]}, Category; #{story[:category]}, Upvotes: #{story[:upvotes]}"
 end 
 
-
-#this calls prompt_new_story and based on the title will calculate new upvotes value and story title, category, and upvotes and return the latest hash
-def get_upvote_value
-	story = prompt_new_story
-	story[:upvotes] = 0
-	if story[:title].downcase.include? "hey"
-		story[:upvotes] += 5
-	elsif story[:title].downcase.include? "bacon"
-		story[:upvotes] += 10
-	else story[:upvotes] = 0
+def display_all_stories(stories)
+	stories.each do |story|
+		puts "story: #{story[:title]}, category: #{story[:category]}, upvotes: #{story[:upvotes]}"
 	end 
-	return story 
 end 
 
-def display_new_story
-story = get_upvote_value
-print " New Story Added! #{story[:title]}, Category: #{story[:category]}, Upvotes: #{story[:upvotes]} "
+
+stories = []
+puts "Welcome to teddit!  Get today's news tomorrow"
+
+puts "would you like to add a story?"
+answer = gets.chomp
+
+loop do 
+	if answer.downcase != "yes"
+	break
 end 
 
-display_new_story
 
+puts "What is your title:"
+story =  {title: gets.chomp}
+puts "Please give it a category:"
+story[:category] = gets.chomp
+get_upvotes(story)
+stories << story
+display_new_story(story)
+puts "Would you like to add another story"
+answer = gets.chomp
 
+end 
 
-
-
-
-
-
-
+display_all_stories(stories)
 
 
 
