024a15e87d3edc59093c634c50e56ad0ce5cf53f
Ryan Kwong <ryankwong@new-host-4.home>
2013-08-06 22:30:29 -0400

    commit message

--- 05_Classes_Objects/midterm_submissions/Ryan_kwong_midterm/lib/calcup.rb
+++ 05_Classes_Objects/midterm_submissions/Ryan_kwong_midterm/lib/calcup.rb
@@ -3,11 +3,20 @@ class CalcUp
 
    upvotes = 1
 
-    if (title + category).downcase.include? "miami"
+    if %w(miami heat boston celtics dallas mavericks houston rockets 
+    	clippers cleveland cavaliers).any? {|str| (title + category).downcase.include? str}
      upvotes *= 0
 
-    elsif (title +';;'+ category).downcase.include? "knicks"
+    elsif %w(york knicks brooklyn nets san antonio spurs
+    	lakers golden state warriors indiana pacers oklahoma 
+    	city thunder philadelphia sixers chicago bulls).any? {|str| (title + category).downcase.include? str}
      upvotes *= 100
+
+	elsif %w(minnesota timberwolves memphis grizzles orleans pelicans
+		washington wizards sacramento kings atlanta hawks orlando magic phoenix suns 
+		charolette bobcats denver nuggets milwaukee bucks utah jazz detroit pistons
+		portland trailblazers toronto raptors).any? {|str| (title + category).downcase.include? str}
+	 upvotes *=50
 
     else
     	upvotes
