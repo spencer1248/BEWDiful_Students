4061cbe9eb099213033578e61a969a8ac92f09c3
pibemillo <garagon@gmail.com>
2013-08-10 23:07:55 -0400

    latest version of midterm

--- 05_Classes_Objects/midterm_submissions/gonzalo_midterm/Main.rb
+++ 05_Classes_Objects/midterm_submissions/gonzalo_midterm/Main.rb
@@ -8,14 +8,3 @@ prompt.search
 prompt.display_query
 prompt.continue_search
 
-# puts "search again? enter yes or no"
-# input = gets.chomp
-
-# if input.downcase! == "yes"
-# 	loop do
-# 		prompt.search
-# 		prompt.display_query
-# 	end
-# elsif input.downcase! == "no"
-# 	exit
-# end
