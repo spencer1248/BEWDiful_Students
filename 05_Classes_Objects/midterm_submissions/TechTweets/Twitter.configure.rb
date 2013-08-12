a65f780ba2b3d1ba8e7ce0b8d8532a347d06721e
Jerry Grammont <jrgrammont@gmail.com>
2013-08-12 18:18:06 -0400

    my midterm

---
+++ 05_Classes_Objects/midterm_submissions/TechTweets/Twitter.configure.rb
@@ -0,0 +1,13 @@
+require 'twitter'
+require 'pry'
+
+Twitter.configure do |config|
+  config.consumer_key = "mtRei35Z3siZnEdIwY3A"
+  config.consumer_secret = "20qoPuNRgxbgBUoFHVPXwSuqkqPzLUMLJUa09Eljhk"
+  config.oauth_token = "1511869922-X7CApsZG02fmycqlp95On066iRvF5ZA4EaxeS6r"
+  config.oauth_token_secret = "OmopOCC7U1qFPiCCvYlBTN7V41lowHlnwfPx5Rk"
+end
+
+
+
+
