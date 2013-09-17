23b56159cab6ba3d87affcfed9b4fca91ff996fd
msjrandall <msjrandall@aol.com>
2013-09-17 10:01:18 -0400

    Added Class 10 stuff

---
+++ 10_Forms/shirts_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShirtsApp::Application.config.secret_key_base = '6fdaf09fa79f9d8604f869ba7d18f8d3c6ae167f8e0c2e7d197ad3c36864729489c74f1db9ea46b6a39de3a7b6a9af7ce348c876d67495365932bc081ca84bcc'
